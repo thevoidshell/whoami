ebacdea11802425380998571e91e5cdf690bc5ea
Rachel2004 <ray.gupta2004@gmail.com>
2026-06-15 16:58:49 +0530

    about automation

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,6 +1,6 @@
 export const socials = {
   github: "https://github.com/thevoidshell",
   linkedin: "https://www.linkedin.com/in/rachel-gupta-rg2004",
-  email: "ray.gupta2004@gmail.com",
-  discord: ".ray04#6675"
+  email: "mailto:ray.gupta2004@gmail.com",
+  discord: "https://discord.com/users/.ray04#6675"
 };
